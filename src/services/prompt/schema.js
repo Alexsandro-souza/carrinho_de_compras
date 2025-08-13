719fa471e792553888c3538ebf4ea4b6764ba29d
alexsandro.souza <alexsandrosouzasilva68@gmail.com>
2025-08-12 21:43:26 -0300

    Feat: add news features

--- src/services/prompt/schema.js
+++ src/services/prompt/schema.js
@@ -12,23 +12,44 @@ const schemas = [
   },
   {
     message: 'Descrição do produto: ',
-    validate: /^[\p{L}0-9., ]+$/u,
+    validate: /^[\p{L}0-9.,\- ]+$/u,
     error: 'ERRO: A descrição não deve conter caracteres especiais',
   },
   {
     message: 'Valor do produto: ',
     validate: /^\d+(\.\d+)?$/,
-    error: 'ERRO: O valor do produto deve separar os centavos ponto.',
+    error: 'ERRO: Separe os centavos com ponto.',
   },
   {
-    message: 'Escolha o número do item que deseja adicionar o carrinho.',
+    message: 'Quantidade: ',
+    validate: /^[1-9][0-9]*$/,
+    error: 'ERRO: Escolha uma quantidade válida',
+  },
+  {
+    message:'Escolha o número do item que deseja adicionar o carrinho: ',
     validate: /^[0-9]$/,
     error: 'ERRO: Digite apenas números.',
   },
   {
     message:
-      'Deseja adicionar mais algum item ao carrinho?\nDigite 1 para sim\nDigite 2 para não',
+      'Deseja adicionar mais algum item ao carrinho?\nDigite 1 para sim\nDigite 2 para não\n\nOpção: ',
     validate: /^[1-2]$/,
+    error: 'ERRO: Digite apenas números.',
+  },
+  {
+    message: 'Deseja fazer alguma mudança no carrinho: ',
+    validate: /^[sn]$/,
+    error: 'ERRO: Digite apenas s ou n',
+  },
+  {
+    message:
+      '\nDeseja remover algum item do carrinho - Digite 1 \nDeseja alterar a quantidade de algum item do carrinho - Digite 2 \n\nOpção: ',
+    validate: /^[123]$/,
+    error: 'ERRO: Escolha 1 ou 2 \n______________________________\n\n',
+  },
+  {
+    message: 'Número do item: ',
+    validate: /^[0-9]+$/,
     error: 'ERRO: Digite apenas números.',
   },
 ];
--- src/services/utils/utils.js
+++ src/services/utils/utils.js
@@ -1,14 +1,25 @@
 import { toRead } from '../conectionDB/conectionDb.js';
 
-export const showItens = async (dbPath) => {
+const showItens = async (dbPath) => {
   const items = await toRead(dbPath);
   console.log('====================');
   items.forEach((item, index) => {
     console.log(`\nItem: ${index + 1}`);
     console.log(
-      `Produto: ${item.name}\nPreço: ${item.description}\nPreço: ${item.price}`
+      `Produto: ${item.name}\nDescrição: ${
+        item.description
+      }\nPreço: ${formatCoin(item.price)}\nQuantidade: ${item.amount}`
     );
     console.log(`___________________________________________\n\n`);
   });
   return items;
 };
+
+const formatCoin = (value) => {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+};
+
+export { showItens, formatCoin };
